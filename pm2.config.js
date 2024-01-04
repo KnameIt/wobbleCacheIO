1955be9ace811eea5c78b6f7cc5e302b26ee69d8
Knameit <info@knameit.com>
2024-01-04 12:49:51 -0500

    config

--- pm2.config.js
+++ pm2.config.js
@@ -8,11 +8,11 @@ module.exports = {
         max_memory_restart: '1G',
         env: {
             NODE_ENV: 'development',
-            PORT: 3000,
+            PORT: 3002,
         },
         env_production: {
             NODE_ENV: 'production',
-            PORT: 3000,
+            PORT: 3002,
         },
     }],
 };
