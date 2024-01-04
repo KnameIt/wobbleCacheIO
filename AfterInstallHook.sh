4b3bf1868edbf3c123fc5a2bfd6cc87967167c97
Knameit <info@knameit.com>
2024-01-04 10:22:50 -0500

    pm2 and test folder added

--- AfterInstallHook.sh
+++ AfterInstallHook.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 source ~/.bashrc
+
+# Ensure the directory /home/ubuntu/test exists
+if [ ! -d "/home/ubuntu/test" ]; then
+  mkdir -p /home/ubuntu/test
+fi
+
+# Change the owner of the directory
 sudo chown -R ubuntu:ubuntu /home/ubuntu/test
+
+# Navigate to the directory
 cd /home/ubuntu/test
+
+# Check if PM2 is installed, if not, install it
+if ! command -v pm2 &> /dev/null
+then
+    echo "PM2 not found, installing..."
+    npm install -g pm2
+fi
+
+# Install npm packages and start the application
 npm install
 npm run start
-
