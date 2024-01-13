586f4b01ec8527010e6ad8ab944a26224ef8c784
Sujit Das <sujit.mrsc12@gmail.com>
2024-01-13 14:23:21 +0530

    update the lamdaresponse

--- src/server/server.js
+++ src/server/server.js
@@ -949,6 +949,8 @@ io.on("connection", (socket) => {
   socket.on("lamdaResponse", async (data) => {
     console.log("come to the lamda response ====================>", data);
     const SEARCH_ID = Object.keys(data)[0];
+
+	console.log("this is search id ")
     if (apiData.hasOwnProperty(SEARCH_ID)) {
       apiData[SEARCH_ID].result.concat(data[SEARCH_ID]);
       apiData[SEARCH_ID].supplied =
@@ -989,21 +991,7 @@ io.on("connection", (socket) => {
 
   //stream code end
 
-  socket.on("lamdaResponse", async (data) => {
-    console.log("come to the lamda response ====================>", data);
-    const SEARCH_ID = Object.keys(data)[0];
-
-    console.log("search id ==============>", SEARCH_ID);
-    if (apiData.hasOwnProperty(SEARCH_ID)) {
-      apiData[SEARCH_ID].result.concat(data[SEARCH_ID]);
-      apiData[SEARCH_ID].supplied =
-        apiData[SEARCH_ID].supplied + data[SEARCH_ID].supplied;
-    } else {
-      apiData[SEARCH_ID] = {};
-      apiData[SEARCH_ID] = data["dataPayload"];
-      apiData[SEARCH_ID].result = [];
-    }
-  });
+  
 });
 
 const PORT = process.env.PORT || 3005;
